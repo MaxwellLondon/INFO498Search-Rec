1fae18d42d2a2f7e73a95a028ccf111cae1ffb31
MaxwellLondon <maxwelllondon99@gmail.com>
2023-12-03 16:00:24 -0800

    adding server code and front-end test

--- INFO-498/src/App.jsx
+++ INFO-498/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(`http://127.0.0.1:5000/search?query=${query}`);
+      const data = await response.json();
+      setResults(data.results);
+    } catch (error) {
+      console.error('Error fetching search results:', error);
+    }
+  };
 
   return (
-    <>
+    <div>
+      <h1>Search Endpoint Tester</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input
+          type="text"
+          placeholder="Enter your search query"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      {results.length > 0 && (
+        <div className="search-results">
+          <h2>Search Results:</h2>
+          <ul>
+            {results.map((result) => (
+              <li key={result.docid}>{result.content}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ backend/server.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+#import pyterrier as pt
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/search', methods=['GET'])
+def search():
+    # Get the query from the request parameters
+    query = request.args.get('query', '')
+
+    # Perform the search using PyTerrier
+
+    #Dummy result
+    results = [{'docid': '001', 'score': 0.9, 'content': 'Dummy document 1'},
+               {'docid': '002', 'score': 0.8, 'content': 'Dummy document 2'}]
+
+    # Format the results and send them to the front-end
+    response = {'query': query, 'results': results}
+    
+    return jsonify(response)
+
+# You can define more endpoints for recommendation, etc.
+
+if __name__ == '__main__':
+    # Run the Flask app on a specific port (e.g., 5000)
+    app.run(port=5000, debug=True)
