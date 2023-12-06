04a819d9f357adef2b1f5546463dde232146f745
MaxwellLondon <maxwelllondon99@gmail.com>
2023-12-05 21:25:03 -0800

    general work to application

--- INFO-498/src/App.jsx
+++ INFO-498/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import './App.css'; // Include your custom styles
+import 'bootstrap/dist/css/bootstrap.min.css'; // Include Bootstrap styles
+import { FaFolder, FaSearch } from 'react-icons/fa'; // Import icons
 
 function App() {
   const [query, setQuery] = useState('');
@@ -8,34 +11,77 @@ function App() {
     try {
       const response = await fetch(`http://127.0.0.1:5000/search?query=${query}`);
       const data = await response.json();
-      setResults(data.results);
+
+      const docnos = data.results.map(result => result.docno);
+
+      setResults(docnos);
     } catch (error) {
       console.error('Error fetching search results:', error);
     }
   };
 
   return (
-    <div>
-      <h1>Search Endpoint Tester</h1>
-      <div>
-        <input
-          type="text"
-          placeholder="Enter your search query"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <button onClick={handleSearch}>Search</button>
-      </div>
-      {results.length > 0 && (
-        <div className="search-results">
-          <h2>Search Results:</h2>
-          <ul>
-            {results.map((result) => (
-              <li key={result.docid}>{result.content}</li>
-            ))}
-          </ul>
+    <div className="container-fluid h-100">
+      {/* Top Sidebar */}
+      <div className="row bg-dark text-white p-3">
+        <div className="col">
+          <span className="h2">🔍 Search App</span>
         </div>
-      )}
+      </div>
+
+      {/* Main Content */}
+      <div className="row mt-4 h-100">
+        {/* Left Sidebar */}
+        <div className="col-2 bg-light p-3">
+          {/* Folder Icon */}
+          <div className="text-center">
+            <FaFolder size={40} color="#007BFF" />
+            <p className="mt-2">Sidebar Content</p>
+          </div>
+        </div>
+
+        {/* Main Content Area */}
+        <div className="col-10 d-flex flex-column">
+          <h1 className="mb-4">Search Endpoint Tester</h1>
+
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter your search query"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <div className="input-group-append">
+              <button className="btn btn-primary btn-lg" onClick={handleSearch}>
+                <FaSearch /> Search
+              </button>
+            </div>
+          </div>
+
+          {results.length > 0 && (
+            <div className="search-results mt-4">
+              <h2>Search Results:</h2>
+              <table className="table table-bordered table-hover">
+                <thead className="thead-dark">
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Document Number</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {results.map((result, index) => (
+                    <tr key={result}>
+                      <th scope="row">{index + 1}</th>
+                      <td>{result}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-#import pyterrier as pt
+import pyterrier as pt
+import pandas as pd
 
 app = Flask(__name__)
 CORS(app)
@@ -13,14 +14,34 @@ def search():
 
     # Perform the search using PyTerrier
 
-    # Dummy result
-    results = [{'docid': '001', 'score': 0.9, 'content': 'Dummy document 1'},
-               {'docid': '002', 'score': 0.8, 'content': 'Dummy document 2'}]
+    if not pt.started():
+        pt.init()
+
+    index = pt.IndexFactory.of("./backend/polygon_index2")
+
+    lm = pt.BatchRetrieve(index, wmodel="Hiemstra_LM")
+
+    results_df = lm.transform(query)
+
+    # Extract the top 10 results
+    top_results = results_df.head(10)
+
+     # Format the top results into a list of dictionaries
+    formatted_results = []
+    for index, row in top_results.iterrows():
+        result_dict = {
+            'docid': str(row['docid']),
+            'rank': int(row['rank']),
+            'score': float(row['score']),
+            'docno': str(row['docno']),
+        }
+        formatted_results.append(result_dict)
 
     # Format the results and send them to the front-end
-    response = {'query': query, 'results': results}
+    response = {'query': query, 'results': formatted_results}
     
     return jsonify(response)
+
 
 # You can define more endpoints for recommendation, etc.
 
